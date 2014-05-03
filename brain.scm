6ef2212ce5cd9fd01f8f96c638d3b08afbff6b2c
Alex Willisson <atw@mit.edu>
2014-05-02 21:08:52 -0400

    now i'm the most recent commiter

--- brain.scm
+++ brain.scm
@@ -1,18 +1,4 @@
-;; should evaluate everything it can, and simplify afterwards.
-
-;; (define env (make-top-level-environment))
-
-;; (define (eval-and-simplify expr)
-;;   (if (pair? expr)
-;;       (if (evalable? expr)
-;; 	  (eval expr env)
-;; 	  (cons (car expr) (map eval-and-simplify (cdr expr))))
-;;       expr))
-
-;; (define (evalable? expr)
-;;   (if (pair? expr)
-;;       (every (lambda (x) x) (map evalable? expr))
-;;       (or (not (symbol? expr)) (environment-bound? env expr))))
+;; our code ~atw
 
 (define *the-empty-environment* (make-top-level-environment))
 
@@ -30,6 +16,7 @@
 	  (eval expr env)
 	  expr)))
 
+;; borrowing heavily (well, stolen) from ps3
 (define algebra-simplifier
   (rule-simplifier
    (list
