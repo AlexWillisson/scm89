07849c2b31008312f6ae98fe560de3a6b8f117c1
Vineel Adusumilli <vineel@adusumilli.org>
2014-05-14 15:24:45 -0400

    Added left associativity.

--- parser.scm
+++ parser.scm
@@ -40,6 +40,15 @@
 		(lp (car rest) (cdr rest))
 		'()))))))
 
+(define (p:tokenize symbol token)
+  (p:apply car (p:sequence (p:return token) (p:symbol symbol))))
+
+(define none
+  (p:tokenize "" '()))
+
+(define (p:opt parser)
+  (p:choice parser none))
+
 (define (p:any . symbols)
   (apply p:choice (map p:symbol symbols)))
 
@@ -68,12 +77,65 @@
 		(list results input)
 		'()))))))
 
+(define (insert-left! tree value)
+  (if (pair? tree)
+      (if (list? (cdr tree))
+	  (begin
+	    (set-car! tree (list (list)))
+	    (set-cdr! tree value))
+	  (insert-left! (car tree) value))))
+
+(define (flip-tree tree)
+  (let ((flipped (list (list))))
+    (let lp ((tree tree))
+      (if (pair? tree)
+	  (begin
+	    (lp (cdr tree))
+	    (pp (car tree))
+	    (insert-left! flipped (car tree)))))
+    flipped))
+
+(define (flatten tree)
+  (cond ((null? tree) '())
+	((not (pair? tree)) (list tree))
+	(else (append (flatten (car tree))
+		      (flatten (cdr tree))))))
+
+(define (associate tree)
+  (if (null? (car tree))
+      '()
+      (if (null? (cdar tree))
+	  (cdr tree)
+	  (let ((associated (associate (caar tree))))
+	    (list (cdar tree) associated (cdr tree))))))
+
+(define (left-associate tree)
+  (associate (flip-tree tree)))
+
+(define plus
+  (p:tokenize "+" '+))
+
+(define minus
+  (p:tokenize "-" '-))
+
 (define digit
   (p:any "0" "1" "2" "3" "4" "5" "6" "7" "8" "9"))
 
 (define number
   (p:apply string->number (p:apply stitch (p:many digit 1))))
 
-(number "123456 hello")
+(define (id input) input)
+
+(define expr
+  (p:apply flatten
+	   (p:sequence number
+		       (p:opt (p:sequence (p:choice plus minus)
+					  (lambda (input) (expr input)))))))
+
+(define parse
+  (p:apply left-associate expr))
+
+(parse "3+4-5")
+
 
 
